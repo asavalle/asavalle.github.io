ad29901194c2ba788e8d53073794e7963a65666b
anthony avalle <anthonyavalle@hotmail.com>
2023-02-17 15:02:59 -0500

    Deleted unused files.

--- index.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("btn1");
-
-function loadPage(){
-        const url = window.location.href.replace("cover.html","index.html");
-        window.location.href = url;
-}
